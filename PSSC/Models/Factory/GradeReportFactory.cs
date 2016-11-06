e26a6ffbbb154f522cf96e1dcda0e479fae025da
HCarrina <carina.hategan@ligaac.ro>
2016-11-06 22:59:03 +0200

    Tema2
    Factories

---
+++ PSSC/Models/Factory/GradeReportFactory.cs
@@ -0,0 +1,30 @@
+﻿using Models.Generics;
+using Models.Generics.Exceptions;
+using Models.Student;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Models.Factory
+{
+    public class GradeReportFactory
+    {
+
+        public static readonly GradeReportFactory Instance = new GradeReportFactory();
+
+        public GradeReportFactory()
+        {
+
+        }
+
+        public Student.GradeReport createInstance(Student.GradeReport gradeReport)
+        {
+            return gradeReport;
+        }
+    }
+}    
+
+
+
